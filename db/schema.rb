84b9ffb191238381aea042babb425e9de8ae2cd1
piroadams <58477331+piroadams@users.noreply.github.com>
2019-12-11 15:38:01 +0100

    Added association for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191211131131) do
+ActiveRecord::Schema.define(version: 20191211143248) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20191211131131) do
     t.string "User"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "plan_id"
     t.index ["email"], name: "index_models_on_email", unique: true
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
   end
